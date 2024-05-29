4862e3db1aa299d402239d772af5cbbe2b03934f
dekomooca <deko_mooca@hotmail.com>
2024-05-29 19:03:14 -0300

    lista de filmes recentes

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -7,10 +7,10 @@ from .models import Filme
 def lista_filmes_recentes(request):
     #-data_criacao: order by desc
     #data_criacao: order by asc
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:6]
+    lista_filmes_recentes = Filme.objects.all().order_by('-data_criacao')[0:6]
     filme_destaque = random.choice(Filme.objects.all())
     return {"filme_destaque": filme_destaque}
-    return {"lista_filmes_recentes": lista_filmes, "filme_destaque":filme_destaque}
+    return {"lista_filmes_recentes": lista_filmes_recentes, "filme_destaque":filme_destaque}
 
 def lista_filmes_emalta(request):
     lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:6]
